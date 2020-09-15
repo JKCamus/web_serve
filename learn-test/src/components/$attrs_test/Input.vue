bb2c731d1e9651da9d944b2f6d120d2dd38e9a4d
JKCamus <924850758@qq.com>
2020-09-15 13:15:06 +0800

    试验田

---
+++ learn-test/src/components/$attrs_test/Input.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <label>输入框</label>
+        <el-input v-model="inputValue" v-bind="$attrs"></el-input>
+    </div>
+</template>
+<script>
+export default {
+    name: "Input",
+    data() {
+        return {
+            inputValue: "",
+        };
+    },
+};
+</script>
---
+++ learn-test/src/components/$attrs_test/Parent.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <Input placeholder="请输入穿透的placeHolder" :clearable="true" />
+    </div>
+</template>
+<script>
+import Input from "./Input";
+export default {
+    name: "Parent",
+    components: {
+        Input,
+    },
+};
+</script>
