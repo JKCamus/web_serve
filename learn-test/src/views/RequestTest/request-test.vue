dc2b402e900d7e54a5f15b6359db4062ffc798b6
JKCamus <924850758@qq.com>
2021-02-09 09:23:49 +0800

    update

--- learn-test/src/views/RequestTest/request-test.vue
+++ learn-test/src/views/RequestTest/request-test.vue
@@ -4,17 +4,55 @@
  * @Author: camus
  * @Date: 2021-02-05 11:00:02
  * @LastEditors: camus
- * @LastEditTime: 2021-02-05 11:00:17
+ * @LastEditTime: 2021-02-05 13:54:08
 -->
 <template>
-    <div class="request-test">
-      ===============
-    </div>
+  <div class="request-test">===============</div>
 </template>
+
 <script>
+import { getPhotoList } from "services/home";
 export default {
-    name:"request-test",
-}
+  name: "request-test",
+  data() {
+    return {
+      infoList: [],
+    };
+  },
+  methods: {
+    async fetchList() {
+      const list = await getPhotoList(1, 20);
+      this.infoList = list;
+      this.concurrentFetch();
+    },
+    imgPromise(src) {
+      return new Promise((resolve, reject) => {
+        const img = new Image();
+        img.onload = () => resolve();
+        img.onerror = () => reject();
+        img.src = src;
+      });
+    },
+    async concurrentFetch() {
+      function widthAwait(promise) {
+        return promise
+          .then((data) => [null, data])
+          .catch((err) => {
+            return [err, undefined];
+          });
+      }
+      try {
+        const promiseArr = this.infoList.map((item) => widthAwait(this.imgPromise(item.url)));
+      const res=  await Promise.all(promiseArr);
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
+  mounted() {
+    this.fetchList();
+  },
+};
 </script>
 <style lang='scss' scoped>
 </style>
